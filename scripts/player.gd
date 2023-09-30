58e21262e9e14f8ae0ec16a89d5984c901d244e3
Will202X <wepharder@gmail.com>
2023-09-30 15:11:15 -0500

    Added player to level 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,4 +8,6 @@ var directionFacing = Vector2(0, 1)
 var currentDirection = 1
 var otherDirection = 0
 
-
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/StateMachine/playback")
