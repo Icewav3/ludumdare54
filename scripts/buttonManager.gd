82f527ad4039744331eaf1faf4579fa29a3db3e5
Icewav3 <coledorman12@gmail.com>
2023-10-01 16:43:09 -0500

    butonb stuff

--- scripts/buttonManager.gd
+++ scripts/buttonManager.gd
@@ -1,8 +1,9 @@
 extends Node
 var children = []
 var buttonlist = []
-var buttonsolution [2,5,3,9,1,4]
-var buttonsolution [2,5,3,9,1,4]
+var buttonsolution = [2,5,3,9,1,4]
+var playersolution = []
+var index = []
 
 # creates a button list
 func _ready():
@@ -14,6 +15,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-func
+	'''for v in buttonlist:
+		if len(playersolution) > 6:
+			playersolution = []
+		if v.is_pressed:
+			index = buttonlist.find(v)
+			playersolution.append(int(index))
+			print(playersolution)'''
+			
