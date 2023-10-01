6710293a3c2f843004ff3c34d1dca153c1265a8f
AivriX <81509337+AiviriX@users.noreply.github.com>
2023-10-01 23:11:33 +0800

    Dialogue Stuff

--- scripts/DialogueController.gd
+++ scripts/DialogueController.gd
@@ -8,12 +8,11 @@ var img = {"mcnormal": preload  ("res://sprites/Mc placeholder sprite1.png"),
 		
 #idk a way to create simulate a visual novel dialogue without creating an entire library so imma put it in 
 #an array for now. We can just itera
-var dialogue1 = ["Oh look an elevator", "Sweet"]
+var dialogue1 = ["Oh look an elevator", "crap"]
 var dialogue2 = ["The risk I took was calculated but man.", "I am bad at math"]
 
 var dialogIndex = 0
 var finished = false
-
 
 @onready var sprite = $mc
 @onready var dbox = $DialogBox
@@ -23,25 +22,36 @@ func _ready():
 	dbox.visible = false;
 	
 func _process(delta):
-		if Input.is_key_pressed(KEY_O):
-			sprite.texture = texture2
-			dbox.visible = true;
-			load_dialog()
-		if Input.is_key_pressed(KEY_P):
-			sprite.texture = texture1
-			dbox.visible = true;
-		if Input.is_key_pressed(KEY_I):
-			if sprite.visible:
-				sprite.visible = false 
+	load_dialog()
+	
+func _input(event) :
+	if event.is_action_pressed("ui_page_up"):
+		sprite.texture = img["mcscare"]
+		sprite.visible = true;
+		dbox.visible = true;
+	if event.is_action_pressed("ui_page_down"):
+		sprite.texture = img["mcnormal"]
+		sprite.visible = true;
+		dbox.visible = true;
+	if event.is_action_pressed("ui_end"):
+		if sprite.visible:
+			sprite.visible = false 
+			dbox.visible = false
+	
+	if dbox.visible:
+		if event.is_action_pressed("ui_accept"):
+			dialogIndex+=1
+			if dialogIndex == dialogue1.size():
 				dbox.visible = false
-			else: 
-				sprite.visible = true #???
-				
-
+				sprite.visible = false
+				dialogIndex=0
 
 
 func load_dialog():
 	if dialogIndex < dialogue1.size():
 		$DialogBox/RichTextLabel.bbcode_text = dialogue1[dialogIndex]
-	dialogIndex += 1
-
+		if Input.is_key_pressed(KEY_L):
+			dialogIndex+=1
+			$DialogBox/RichTextLoabel.bbcode_text = dialogue1[dialogIndex]
+		print (dialogue1[dialogIndex][dialogIndex])
+	
