04adfe887a6016fb08b5177ed9a26c3064993f69
Will202X <wepharder@gmail.com>
2023-10-01 15:48:02 -0500

    Changed volume

--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var state
 @onready var startButton = $StartButton
+@onready var effect = get_tree().root.get_node("GlobalScene/EffectsPlayer")
 var globalScene = preload("res://scripts/StateManager.gd")
 
 func _ready():
 	state = globalScene.get_data()
+	effect.play_sound("hum")
 
 func _process(delta):
 	pass
