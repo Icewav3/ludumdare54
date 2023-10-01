2762af7df90519d157f04ede1f0216f2b53be1cf
Icewav3 <coledorman12@gmail.com>
2023-09-30 19:47:00 -0500

    Intellij integration

--- scripts/StateManager.gd
+++ scripts/StateManager.gd
@@ -2,16 +2,12 @@ extends Node2D
 
 @export var debug : bool
 
-var levels = {}
 var state = {"menulevel": preload("res://scenes/MainMenu.tscn"), "level1": preload("res://scenes/level_1.tscn"), "level2": preload("res://scenes/level_2.tscn"), "level3": preload("res://scenes/level_3.tscn")}
-var currentState
+var currentState = state["menulevel"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(state["menulevel"].instantiate())
-	'''if Input.
-		if(debug):
-			print(state["menulevel"])'''
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
